0c8b87765fc591fa4afa740135f3c8d995169be9
Statev07 <petiostatev2703@gmail.com>
2021-11-26 15:42:52 +0200

    Add validation to team database entity

--- SoccerAPI/Database/SoccerAPI.Database.Models/Teams/Team.cs
+++ SoccerAPI/Database/SoccerAPI.Database.Models/Teams/Team.cs
@@ -2,7 +2,9 @@
 {
     using System;
     using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
 
+    using SoccerAPI.Common.Constants.ModelConstants;
     using SoccerAPI.Database.Models.BaseModels;
 
     public class Team : BaseModel
@@ -15,12 +17,23 @@
             this.Championships = new HashSet<TeamChampionshipMapping>();
         }
 
+        [Required]
+        [MinLength(TeamConstants.NAME_MIN_LENGHT)]
+        [MaxLength(TeamConstants.NAME_MAX_LENGHT)]
         public string Name { get; set; }
 
+        [Required]
+        [MinLength(TeamConstants.NICKNAME_MIN_LENGHT)]
+        [MaxLength(TeamConstants.NICKNAME_MAX_LENGHT)]
         public string Nickname { get; set; }
 
+        [Required]
+        [MinLength(TeamConstants.STADIUM_MIN_LENGHT)]
+        [MaxLength(TeamConstants.STADIUM_MAX_LENGHT)]
         public string Stadium { get; set; }
 
+        [Required]
+        [MaxLength(TeamConstants.COUNTRY_MAX_LENGHT)]
         public string Country { get; set; }
 
         public virtual ICollection<TeamCoachMapping> Coaches { get; set; }
