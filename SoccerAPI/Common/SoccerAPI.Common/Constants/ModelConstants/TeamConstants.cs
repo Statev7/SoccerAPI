a37adb5834f2f303187fe7b0b6ef68342a7f1ce2
Statev07 <petiostatev2703@gmail.com>
2021-11-26 15:41:19 +0200

    Create new folder for app commons

---
+++ SoccerAPI/Common/SoccerAPI.Common/Constants/ModelConstants/TeamConstants.cs
@@ -0,0 +1,16 @@
+﻿namespace SoccerAPI.Common.Constants.ModelConstants
+{
+    public static class TeamConstants
+    {
+        public const int NAME_MIN_LENGHT = 3;
+        public const int NAME_MAX_LENGHT = 64;
+
+        public const int NICKNAME_MIN_LENGHT = 3;
+        public const int NICKNAME_MAX_LENGHT = 64;
+
+        public const int STADIUM_MIN_LENGHT = 3;
+        public const int STADIUM_MAX_LENGHT = 128;
+
+        public const int COUNTRY_MAX_LENGHT = 64;
+    }
+}
