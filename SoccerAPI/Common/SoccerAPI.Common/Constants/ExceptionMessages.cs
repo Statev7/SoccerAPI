c59a744726bdeecb0cc6d6a2d1e422f52708aab6
Statev07 <petiostatev2703@gmail.com>
2021-12-22 12:17:47 +0200

    Update ExceptionMessages.cs

--- SoccerAPI/Common/SoccerAPI.Common/Constants/ExceptionMessages.cs
+++ SoccerAPI/Common/SoccerAPI.Common/Constants/ExceptionMessages.cs
@@ -6,17 +6,24 @@
 
         public const string FOOTBALLER_NOT_EXIST_ERROR_MESSAGE = "Such a footballer does not exist!";
 
-        public const string FOOTBALLER_IS_ALREADY_IN_THE_TEAM_ERROR_MESSAGE = "The footballer is already in the team";
+        public const string FOOTBALLER_IS_ALREADY_IN_THE_TEAM_ERROR_MESSAGE = "{0} is already in the team!";
 
-        public const string A_FOOTBALLER_CANNOT_HAVE_MORE_TEAMS_ERROR_MESSAGE = "The footballer has reached the maximum number of teams";
+        public const string A_FOOTBALLER_CANNOT_HAVE_MORE_TEAMS_ERROR_MESSAGE = "The {0} has reached the maximum number of teams!";
 
-        public const string TEAM_PLAYERS_COUNT_ERROR_MESSAGE = "The team cannot have more than {0} players";
+        public const string TEAM_PLAYERS_COUNT_ERROR_MESSAGE = "The team cannot have more than {0} players!";
 
         public const string TEAM_NOT_EXIST_ERROR_MESSAGE = "Such a team does not exist!";
 
         public const string TEAM_IS_ALREADY_IN_THIS_CHAMPIONSHIP_ERROR_MESSAGE = "The {0} is already in {1}!";
 
-        public const string CANNOT_ADD_NEW_TEAM_ERROR_MESSAGE = "The {0} has reached the maximum number of teams";
+        public const string CANNOT_ADD_NEW_TEAM_ERROR_MESSAGE = "The {0} has reached the maximum number of teams!";
 
+        public const string COACH_NOT_EXIST_ERROR_MESSAGE = "Such a coach does not exist!";
+
+        public const string THE_COACH_IS_ALREADY_IN_THE_TEAM = "{0} is already a coach!";
+
+        public const string THE_COACH_IS_ALREADY_THE_COACH_OF_ANOTHER_TEAM = "{0} is the coach of another team";
+
+        public const string CANNOT_ADD_NEW_COACH_TO_TEAM_ERROR_MESSAGE = "The {0} has reached the maximum number of coaches!";
     }
 }
